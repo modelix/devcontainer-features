cdca725811b351a8b62f586959b0953995d93944
Sascha Lisson <sascha.lisson@itemis.de>
2024-11-02 13:58:09 +0100

    run indexer

--- src/mps/run-indexer.sh
+++ src/mps/run-indexer.sh
@@ -5,6 +5,6 @@
 set -x
 set -e
 
-find / -type d -name ".mps" | while read -r dir; do
+find ~/ -type d -name ".mps" | while read -r dir; do
   /mps/bin/mps.sh warmup --project-dir="$(dirname "$dir")"
 done
